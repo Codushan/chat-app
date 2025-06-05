0dfa714fbc08cb4922f80940bdfdd0642b55d8e2
CB <chandrabhushankumar138@gmail.com>
2025-06-05 21:49:02 +0530

    project completed4

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,9 @@
+// postcss.config.js
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: './tailwind.config.js', // Explicitly point to your Tailwind config
+    },
     autoprefixer: {},
-  },
+  }
 }
